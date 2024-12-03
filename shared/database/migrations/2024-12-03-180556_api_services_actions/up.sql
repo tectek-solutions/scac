18ca82f09bdc70c40efe03093fd575df565d0ebd
MathTek <mathieu.rasoanaivo@epitech.eu>
2024-12-03 22:22:33 +0400

    Feat: implement scheme for api action services

---
+++ shared/database/migrations/2024-12-03-180556_api_services_actions/up.sql
@@ -0,0 +1,15 @@
+-- Your SQL goes here
+CREATE TABLE api_services_actions (
+    id SERIAL PRIMARY KEY,
+    api_service_id INT NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    description TEXT,
+    endpoint TEXT NOT NULL,
+    method http_method_enum NOT NULL,
+    headers JSONB,
+    params JSONB,
+    json_path TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (api_service_id) REFERENCES api_services(id)
+);
