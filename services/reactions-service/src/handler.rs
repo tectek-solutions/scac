ee5cf87f2e0413973f065df52c2cfe0d9c3094fb
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2024-12-14 11:18:40 +0400

    Feat: Add Reactions Services

---
+++ services/reactions-service/src/handler.rs
@@ -0,0 +1,95 @@
+use actix_web::{get, web, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+
+use database;
+
+use crate::query;
+
+// ----------------------------
+// Struct Definitions
+// ---------------------------
+
+#[derive(Serialize, Deserialize, Clone, ToSchema)]
+pub enum ErrorResponse {
+    NotFound(String),
+    Unauthorized(String),
+    InternalServerError(String),
+}
+
+impl ErrorResponse {
+    fn to_response(&self, status: actix_web::http::StatusCode) -> HttpResponse {
+        HttpResponse::build(status).json(self)
+    }
+}
+
+// ----------------------------
+// Helper Functions
+// ----------------------------
+
+// ----------------------------
+// Handlers
+// ----------------------------
+
+#[utoipa::path(
+    get,
+    path = "/api-services/{id}",
+    tag = "reactions",
+    responses(
+        (status = 200, description = "List of reactions retrieved"),
+        (status = 403, description = "Unauthorized", body = ErrorResponse),
+        (status = 500, description = "Internal server error", body = ErrorResponse)
+    )
+)]
+#[get("/")]
+async fn list_reactions_by_api_service_id(db: web::Data<database::Database>, id: web::Path<i32>) -> impl Responder {
+    match query::list_reactions_by_api_service_id_query(&db, id.into_inner()) {
+        Ok(Some(reactions)) => HttpResponse::Ok().json(reactions),
+        Ok(None) => ErrorResponse::NotFound("No reactions found".to_string())
+            .to_response(actix_web::http::StatusCode::NOT_FOUND),
+        Err(err) => {
+            eprintln!("Error getting reactions: {:?}", err);
+            ErrorResponse::InternalServerError("Failed to get reactions".to_string())
+                .to_response(actix_web::http::StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
+}
+
+#[utoipa::path(
+    get,
+    path = "/{id}",
+    tag = "reactions",
+    responses(
+        (status = 200, description = "Reactions details retrieved"),
+        (status = 404, description = "Reactions ID not found", body = ErrorResponse),
+        (status = 500, description = "Internal server error", body = ErrorResponse)
+    )
+)]
+#[get("/{id}")]
+async fn get_reaction_by_id(
+    db: web::Data<database::Database>,
+    id: web::Path<i32>,
+) -> impl Responder {
+    match query::get_reaction_by_id_query(&db, id.into_inner()) {
+        Ok(Some(action)) => HttpResponse::Ok().json(action),
+        Ok(None) => ErrorResponse::NotFound("Authentification not found".to_string())
+            .to_response(actix_web::http::StatusCode::NOT_FOUND),
+        Err(err) => {
+            eprintln!("Error getting action: {:?}", err);
+            ErrorResponse::InternalServerError("Failed to get action".to_string())
+                .to_response(actix_web::http::StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
+}
+
+// ----------------------------
+// Service Configuration
+// ----------------------------
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/reactions")
+            .service(list_reactions_by_api_service_id)
+            .service(get_reaction_by_id),
+    );
+}
--- services/reactions-service/src/query.rs
+++ services/reactions-service/src/query.rs
@@ -1,15 +1,19 @@
 use actix_web::web;
 use database;
-use database::model::{NewReactions, Reactions};
+use database::model::{Reactions};
 use diesel::prelude::*;
 
-pub fn get_reactions(
+pub fn list_reactions_by_api_service_id_query(
     db: &web::Data<database::Database>,
+    search_id: i32,
 ) -> Result<Option<Vec<Reactions>>, diesel::result::Error> {
     use database::schema::reactions::dsl::*;
 
     let mut connection = db.get_connection();
-    let result = reactions.load::<Reactions>(&mut connection);
+    let result = reactions
+        .filter(api_service_id.eq(search_id))
+        .select(Reactions::as_select())
+        .load::<Reactions>(&mut connection);
 
     match result {
         Ok(result) => Ok(Some(result)),
@@ -20,95 +24,142 @@ pub fn get_reactions(
     }
 }
 
-pub fn get_reactions_by_id(
+pub fn get_reaction_by_id_query(
     db: &web::Data<database::Database>,
-    reaction_id: i32,
+    search_id: i32,
 ) -> Result<Option<Reactions>, diesel::result::Error> {
     use database::schema::reactions::dsl::*;
 
     let mut connection = db.get_connection();
 
     match reactions
-        .find(reaction_id)
+        .find(search_id)
         .select(Reactions::as_select())
         .first::<Reactions>(&mut connection)
         .optional()
     {
-        Ok(Some(reaction)) => Ok(Some(reaction)),
+        Ok(Some(action)) => Ok(Some(action)),
         Ok(None) => Ok(None),
         Err(err) => {
-            eprintln!(
-                "Error getting reaction with ID {:?}: {:?}",
-                reaction_id, err
-            );
+            eprintln!("Error getting action with ID {:?}: {:?}", search_id, err);
             Err(err)
         }
     }
 }
 
-pub fn create_reaction(
-    db: &web::Data<database::Database>,
-    _api_service_id: i32,
-    _name: String,
-    _description: Option<String>,
-    _endpoint: String,
-    _method: String,
-    _headers: Option<serde_json::Value>,
-    _params: Option<serde_json::Value>,
-    _json_path: Option<String>,
-    _updated_at: Option<chrono::NaiveDateTime>,
-) -> Result<Option<Reactions>, diesel::result::Error> {
-    use database::schema::reactions;
+// pub fn create_reactions(
+//     db: &web::Data<database::Database>,
+//     api_service_id: i32,
+//     name: &str,
+//     description: Option<&str>,
+//     endpoint: &str,
+//     method: String,
+//     headers: Option<&serde_json::Value>,
+//     params: Option<&serde_json::Value>,
+//     json_path: Option<&str>,
+//     created_at: Option<NaiveDateTime>,
+//     updated_at: Option<NaiveDateTime>,
+// ) -> Result<Option<Reactions>, diesel::result::Error> {
+//     use database::schema::reactions;
 
-    let new_reaction = NewReactions {
-        api_service_id: _api_service_id,
-        name: &_name,
-        description: _description.as_deref(),
-        endpoint: &_endpoint,
-        method: _method,
-        headers: _headers.as_ref(),
-        params: _params.as_ref(),
-        json_path: _json_path.as_deref(),
-        created_at: Some(chrono::Utc::now().naive_utc()),
-        updated_at: _updated_at,
-    };
+//     let new_action = NewActions {
+//         api_service_id: api_service_id,
+//         name: name,
+//         description: description,
+//         endpoint: endpoint,
+//         method: method,
+//         headers: headers,
+//         params: params,
+//         json_path: json_path,
+//         created_at: created_at,
+//         updated_at: updated_at,
+//     };
 
-    let mut connection = db.get_connection();
+//     let mut connection = db.get_connection();
 
-    match diesel::insert_into(reactions::table)
-        .values(&new_reaction)
-        .get_result::<Reactions>(&mut connection)
-    {
-        Ok(reaction) => Ok(Some(reaction)),
-        Err(err) => {
-            eprintln!("Error creating reaction: {:?}", err);
-            Err(err)
-        }
-    }
-}
+//     match diesel::insert_into(reactions::table)
+//         .values(&new_action)
+//         .returning(Reactions::as_select())
+//         .get_result::<Reactions>(&mut connection)
+//         .optional()
+//     {
+//         Ok(action) => Ok(action),
+//         Err(err) => {
+//             eprintln!("Error creating action: {:?}", err);
+//             Err(err)
+//         }
+//     }
+// }
 
-pub fn delete_reaction(
-    db: &web::Data<database::Database>,
-    reaction_id: i32,
-) -> Result<Option<Reactions>, diesel::result::Error> {
-    use database::schema::reactions::dsl::*;
+// pub fn update_reactions(
+//     db: &web::Data<database::Database>,
+//     action_id: i32,
+//     _api_service_id: i32,
+//     _name: &str,
+//     _description: Option<&str>,
+//     _endpoint: &str,
+//     _method: String,
+//     _headers: Option<&serde_json::Value>,
+//     _params: Option<&serde_json::Value>,
+//     _json_path: Option<&str>,
+//     _created_at: Option<NaiveDateTime>,
+//     _updated_at: Option<NaiveDateTime>,
+// ) -> Result<Option<Reactions>, diesel::result::Error> {
+//     use database::schema::reactions::dsl::*;
 
-    let mut connection = db.get_connection();
+//     let mut connection = db.get_connection();
+//     diesel::update(reactions.find(action_id))
+//         .set((
+//             api_service_id.eq(_api_service_id),
+//             name.eq(_name),
+//             description.eq(_description),
+//             endpoint.eq(_endpoint),
+//             method.eq(_method),
+//             headers.eq(_headers),
+//             params.eq(_params),
+//             json_path.eq(_json_path),
+//             created_at.eq(_created_at),
+//             updated_at.eq(_updated_at),
+//         ))
+//         .execute(&mut connection)?;
 
-    match diesel::delete(reactions.filter(id.eq(reaction_id)))
-        .get_result::<Reactions>(&mut connection)
-        .optional()
-    {
-        Ok(reaction) => Ok(reaction),
-        Err(err) => {
-            eprintln!(
-                "Error deleting reaction with ID {:?}: {:?}",
-                reaction_id, err
-            );
-            Err(err)
-        }
-    }
-}
+//     match reactions
+//         .find(action_id)
+//         .select(Reactions::as_select())
+//         .first::<Reactions>(&mut connection)
+//         .optional()
+//     {
+//         Ok(Some(action)) => Ok(Some(action)),
+//         Ok(None) => Ok(None),
+//         Err(err) => {
+//             eprintln!(
+//                 "Error updating action with ID {:?}: {:?}",
+//                 action_id, err
+//             );
+//             Err(err)
+//         }
+//     }
+// }
 
+// pub fn delete_reactions(
+//     db: &web::Data<database::Database>,
+//     action_id: i32,
+// ) -> Result<Option<Reactions>, diesel::result::Error> {
+//     use database::schema::reactions::dsl::*;
 
+//     let mut connection = db.get_connection();
+//     let deleted_action = diesel::delete(reactions.find(action_id))
+//         .get_result::<Reactions>(&mut connection)
+//         .optional();
 
+//     match deleted_action {
+//         Ok(deleted_action) => Ok(deleted_action),
+//         Err(err) => {
+//             eprintln!(
+//                 "Error deleting action with ID {:?}: {:?}",
+//                 action_id, err
+//             );
+//             Err(err)
+//         }
+//     }
+// }
