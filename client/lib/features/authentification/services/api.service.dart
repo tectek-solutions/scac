7c0908e2cdffa96a29d6aa475f887500b5057499
Lauret Benjamin <benjamin.lauret@epitech.eu>
2024-12-16 15:31:01 +0400

    feat: add api.service for registration, login and logout

--- client/lib/features/authentification/services/api.service.dart
+++ client/lib/features/authentification/services/api.service.dart
@@ -6,18 +6,53 @@ class ApiService {
 
   ApiService({required this.baseUrl});
 
-  Future<Map<String, dynamic>> login(String username, String password) async {
-    final url = Uri.parse('$baseUrl/login');
+  Future<Map<String, dynamic>> signIn(String email, String password) async {
+    final url = Uri.parse('$baseUrl/signin');
     final response = await http.post(
       url,
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'username': username, 'password': password}),
+      body: jsonEncode({'email': email, 'password': password}),
     );
 
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
-      throw Exception('Failed to login');
+      throw Exception('Failed to sign in');
+    }
+  }
+
+  Future<Map<String, dynamic>> signUp(String name, String firstname, String email, String password) async {
+    final url = Uri.parse('$baseUrl/signup');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'name': name,
+        'firstname': firstname,
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to sign up');
+    }
+  }
+
+  Future<void> signOut(String token) async {
+    final url = Uri.parse('$baseUrl/signout');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to sign out');
     }
   }
 }
