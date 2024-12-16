c0bbb713a4111233f4018623d1cd87d98e12eb3b
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2024-12-17 00:14:48 +0400

    Feat: Refactor database

--- shared/database/migrations/2024-12-16-173407_add_workflow/up.sql
+++
@@ -1,5 +0,0 @@
--- Your SQL goes here
-INSERT INTO workflows (user_id, name, description, action_id, reaction_id)
-VALUES 
-(1, 'Gmail Workflow', 'Checks for new mail and sends a reply', 1, 1),
-(4, 'Outlook Workflow', 'Checks for new mail and sends a reply', 2, 2);
