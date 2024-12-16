1ce49119647a8f6d688c7fea084f3cb351a64b95
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2024-12-16 22:17:41 +0400

    Fix: Changes bits to varchar for password_hash

--- tables.sql
+++ tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY, -- Unique ID
     name VARCHAR(32) UNIQUE NOT NULL, -- Unique name
     email VARCHAR(128) UNIQUE NOT NULL, -- Email
-    password_hash BIT(256) NOT NULL, -- Hashed password
+    password_hash VARCHAR(32) NOT NULL, -- Hashed password
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Creation timestamp
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Last update timestamp
 );
