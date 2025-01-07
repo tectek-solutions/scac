7743101934bb68064158dc57287634dfa9691bd7
CarolinHugo <hugo.carolin@epitech.eu>
2025-01-07 19:44:54 +0400

    Feat: add recup token in flutter storage

--- client/lib/features/authentification/services/api.service.dart
+++ client/lib/features/authentification/services/api.service.dart
@@ -1,5 +1,9 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+
+final storage = FlutterSecureStorage();
 
 class ApiAccountService {
   final String baseUrl;
@@ -15,7 +19,9 @@ class ApiAccountService {
     );
 
     if (response.statusCode == 200) {
-      return jsonDecode(response.body);
+      final token = jsonDecode(response.body);
+      await storage.write(key: 'jwt', value: token);
+      return token;
     } else if (response.statusCode == 401) {
       throw Exception('Unauthorized');
     } else if (response.statusCode == 500) {
@@ -39,6 +45,8 @@ class ApiAccountService {
     );
 
     if (response.statusCode == 201) {
+      final token = jsonDecode(response.body)['Ok'];
+      await storage.write(key: 'jwt', value: token);
       return jsonDecode(response.body);
     } else if (response.statusCode == 401) {
       throw Exception('Unauthorized');
@@ -49,18 +57,28 @@ class ApiAccountService {
     }
   }
 
-  Future<void> signOut(String token) async {
+  Future<void> signOut() async {
     final url = Uri.parse('$baseUrl/users/sign_out');
-    final response = await http.post(
+    final response = await http.delete(
       url,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
+      headers: {'Content-Type': 'application/json'},
     );
-
-    if (response.statusCode != 200) {
-      throw Exception('Failed to sign out');
+    if (response.statusCode == 200) {
+      await storage.delete(key: 'jwt');
+    } else if (response.statusCode == 401) {
+      throw Exception('Unauthorized');
+    } else if (response.statusCode == 500) {
+      throw Exception('Internal server error');
+    } else {
+      throw Exception('Error');
     }
+  }
+
+  Future<bool> isTokenExpired() async {
+    final token = await storage.read(key: 'jwt');
+    if (token == null) {
+      return true;
+    }
+    return JwtDecoder.isExpired(token);
   }
 }
