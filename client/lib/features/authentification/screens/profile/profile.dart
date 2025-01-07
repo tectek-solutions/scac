7613cb8ee38e869f344e7cb69bee5f85bcf282d2
CarolinHugo <hugo.carolin@epitech.eu>
2025-01-07 16:57:14 +0400

    Refactoring: add api with profile page but not functionnal (no jwt token for testing)

--- client/lib/features/authentification/screens/profile/profile.dart
+++ client/lib/features/authentification/screens/profile/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:client/features/authentification/services/api.service.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -8,6 +9,27 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final ApiAccountService _apiService = ApiAccountService(baseUrl: baseUrlString);
+  static const baseUrlString = String.fromEnvironment('API_URL', defaultValue: 'http://localhost:8000');
+  Map<String, dynamic>? _userProfile;
+  bool _isLoading = true;
+  bool _hasError = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _apiService.fetchUserProfile(token).then((value) {
+      setState(() {
+        _isLoading = false;
+        if (value is Map<String, dynamic>) {
+          _userProfile = value;
+        } else {
+          _hasError = true;
+        }
+      });
+    });
+  }
+
   void _logout() {
     // Implement your logout functionality here
     print('User logged out');
@@ -24,45 +46,49 @@ class _ProfileState extends State<Profile> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const CircleAvatar(
-                radius: 50,
-                backgroundImage: NetworkImage('https://example.com/profile.jpg'),
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                'John Doe',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                'Email here',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                'add your bio here',
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 150, // Set the desired width
-                height: 60, // Set the desired height
-                child: ElevatedButton(
-                  onPressed: _logout,
-                  child: const Text('Logout'),
-                ),
-              ),
-            ],
-          ),
+          child: _isLoading
+              ? const CircularProgressIndicator()
+              : _hasError
+                  ? const Text('Error loading profile')
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(_userProfile?['profile_image_url'] ?? 'https://example.com/profile.jpg'),
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          _userProfile?['name'] ?? 'John Doe',
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          _userProfile?['email'] ?? 'Email here',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Add your bio here',
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 30),
+                        SizedBox(
+                          width: 150,
+                          height: 60,
+                          child: ElevatedButton(
+                            onPressed: _logout,
+                            child: const Text('Logout'),
+                          ),
+                        ),
+                      ],
+                    ),
         ),
       ),
     );
