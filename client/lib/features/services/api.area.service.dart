f97b553729b39f0035a3ac9ccf4c0b31519c5029
CarolinHugo <hugo.carolin@epitech.eu>
2025-01-15 00:36:22 +0400

    Fix: modify fetch function for all with token

--- client/lib/features/services/api.area.service.dart
+++ client/lib/features/services/api.area.service.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'api.service.dart';
 
 class ApiService {
   final String baseUrl;
@@ -8,17 +9,21 @@ class ApiService {
   ApiService({required this.baseUrl, required this.route});
 
   Future<dynamic> fetchCards() async {
-    print("Fetching cards from $baseUrl$route");
     final url = Uri.parse('$baseUrl$route');
+    final token = getToken();
+    print(token);
     final response = await http.get(
       url,
-      headers: {'Content-Type': 'application/json'},
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
     );
     if (response.statusCode == 200) {
       final dynamic data = jsonDecode(response.body);
       return data;
     } else {
-      throw Exception('Failed to load cards');
+      throw Exception('Failed to fetch');
     }
   }
 }
