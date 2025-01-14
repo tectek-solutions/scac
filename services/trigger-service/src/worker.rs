18934c6cb6946877ffb509ba936d5575c2ce3d83
Clément Antoine XAVIER <clement-antoine.xavier@epitech.eu>
2025-01-14 23:45:11 +0400

    Fix: triggers

--- services/trigger-service/src/worker.rs
+++ services/trigger-service/src/worker.rs
@@ -104,28 +104,45 @@ impl Worker {
 
         let url = format!("{}/{}", action_api.base_url, action.http_endpoint);
 
-        let response = client
-            .request(
-                match reqwest::Method::from_bytes(action.http_method.as_bytes()) {
-                    Ok(method) => method,
-                    Err(err) => {
-                        error!("Error getting method: {:?}", err);
-                        return Err(std::io::Error::new(
-                            std::io::ErrorKind::Other,
-                            "Error getting method",
-                        ));
-                    }
-                },
-                &url,
-            )
+        info!("URL: {}", url);
+
+        let method = match reqwest::Method::from_bytes(action.http_method.as_bytes()) {
+            Ok(method) => method,
+            Err(err) => {
+                error!("Error getting method: {:?}", err);
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Error getting method",
+                ));
+            }
+        };
+
+        let request = client
+            .request(method, &url)
             .form(&action.http_parameters)
             .header(
                 "Authorization",
                 format!("Bearer {}", action_user_token.access_token),
             )
+            .header("Content-Type", "application/json") 
+            .header("Accept", "application/json")
             .json(&action.http_body)
-            .send()
-            .await;
+            .build();
+
+        let request = match request {
+            Ok(request) => request,
+            Err(err) => {
+                error!("Error building request: {:?}", err);
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Error building request",
+                ));
+            }
+        };
+
+        info!("Request: {:?}", request);
+
+        let response = client.execute(request).await;
 
         let response = match response {
             Ok(response) => response,
@@ -138,8 +155,15 @@ impl Worker {
             }
         };
 
-        let data: serde_json::Value = match response.json().await {
-            Ok(data) => data,
+        info!("Response: {:?}", response);
+
+        info!("Response status: {:?}", response.status());
+        info!("Response headers: {:?}", response.headers());
+        let response_text = response.text().await;
+        info!("Response text: {:?}", response_text);
+
+        let data: serde_json::Value = match response_text {
+            Ok(text) => serde_json::from_str(&text).unwrap_or_default(),
             Err(err) => {
                 error!("Error getting json response: {:?}", err);
                 return Err(std::io::Error::new(
@@ -149,7 +173,7 @@ impl Worker {
             }
         };
 
-        println!("Response: {:?}", data);
+        println!("Response: {}", data);
 
         let reaction = match database::model::Reaction::read(
             &mut self.database.get_connection(),
